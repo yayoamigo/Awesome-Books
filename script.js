0af586cff4ac86996b5b54300e9d78f02e5b0240
AbeeraTahir <abeeratahir2018@gmail.com>
2022-10-25 01:33:26 +0500

    Add adding books functionality

--- script.js
+++ script.js
@@ -2,10 +2,11 @@ const body = document.getElementById('displayed-books');
 const addBtn = document.querySelector('.add');
 const titleInput = document.getElementById('title');
 const authorInput = document.getElementById('author');
+const addBooksForm = document.getElementById('add-books');
 
+let booksArr = [];
 
-
-function AddingBook (title,author){
+function AddingBook (){
  //creating a div 
  const booksDiv = document.createElement('div');
  //adding the class books to new div
@@ -13,13 +14,24 @@ function AddingBook (title,author){
  //adding new div to the html page
  body.appendChild(booksDiv);
  //book data
- let title = titleInput.value;
- let author = authorInput.value;
+
+  const newBook = {
+    title: titleInput.value,
+    author: authorInput.value,
+  }
+ booksArr.push(newBook);
+ console.log(booksArr);
+
  //book content
- const content = `<p class="book-title">Lorem ipsum</p>
- <p class="author"> Testeroo testy</p>
+ const content = `<p class="book-title">${newBook.title}</p>
+ <p class="author">${newBook.author}</p>
 <button class="remove-btn"> Remove</button>
 <hr>`;
 //inserting the book content to new div
 booksDiv.insertAdjacentHTML("beforeend", content);
 }
+
+addBooksForm.addEventListener('submit', (e) => {
+  AddingBook();
+  e.preventDefault();
+})
