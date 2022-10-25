8cdbc0168889486f5a5d18921c67d9a048c4e16f
David Zambrano <david._101@hotmail.com>
2022-10-25 09:28:44 -0500

    Fix linter error and update the readme file

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
 # Awesome-Books
-This is a website for adding and removing books
+This is a website for adding and removing books ddynamically using Javascript
+
+## Built With
+- HTML
+- CSS
+# Get Started
+
+ -[Portfolio](https://yayoamigo.github.io/)
+
+
+## Authors
+
+👤 **DavidZambrano**
+
+- GitHub: [@yayoamigo](https://github.com/yayoamigo)
+- Twitter: [@carpediem0011](https://twitter.com/carpediem0011)
+- LinkedIn: [LinkedIn](https://www.linkedin.com/in/david-zambrano-corral-b87a4198/)
+
+👤 **Abeera Tahir**
+
+- GitHub: [@githubhandle](https://github.com/AbeeraTahir)
+- Twitter: [@twitterhandle]( https://twitter.com/AbeeraTahir8?t=z5CjMpmHMZmS98i09gUpYA&s=08)
+- LinkedIn: [LinkedIn](https://www.linkedin.com/in/abeera-tahir-961893176)
+
+
+
+## 🤝 Contributing
+
+Contributions, issues, and feature requests are welcome!
+
+
+
+## Show your support
+
+Give a ⭐️ if you like this project!
+
+## Acknowledgments
+
+
+## 📝 License
+
+This project is MIT licensed.
+
--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 const booksBody = document.getElementById('displayed-books');
 const addBooksForm = document.getElementById('add-books');
 
-let booksArr = [];
+const booksArr = [];
 
-//function for adding a book
-function addBook (newBook){
-  //book content
- const content = `<div class="books"><p id="title-input">${newBook.title}</p>
+// function for adding a book
+function addBook(newBook) {
+  // book content
+  const content = `<div class="books"><p id="title-input">${newBook.title}</p>
  <p class="author">${newBook.author}</p>
  <button class="remove-btn"> Remove</button>
  <hr></div>`;
-  //inserting the book content to new div
-  booksBody.insertAdjacentHTML("beforeend", content);
+  // inserting the book content to new div
+  booksBody.insertAdjacentHTML('beforeend', content);
 }
 
-//function for displaying the books
+// function for displaying the books
 function displayBooks() {
   const books = JSON.parse(localStorage.getItem('books'));
   books.forEach((book) => addBook(book));
@@ -22,31 +22,30 @@ function displayBooks() {
 
 window.addEventListener('DOMContentLoaded', displayBooks);
 
-//function for removing books from local storage
-function removeBooks(book){
+// function for removing books from local storage
+function removeBooks(book) {
   const bookTitle = book.querySelector('#title-input').innerText;
   const b = JSON.parse(localStorage.getItem('books'));
-  const filterIndex = b.findIndex(x => x.title === bookTitle);
+  const filterIndex = b.findIndex((x) => x.title === bookTitle);
   b.splice(filterIndex, 1);
   localStorage.setItem('books', JSON.stringify(b));
 }
 
-//function for removing books from interface
+// function for removing books from interface
 function removeBookUI(element) {
   element.parentElement.remove();
-  
 }
 
 // Element target
 booksBody.addEventListener('click', (e) => {
-    const book = e.target.parentElement;
-    removeBooks(book);
-    removeBookUI(e.target);
+  const book = e.target.parentElement;
+  removeBooks(book);
+  removeBookUI(e.target);
 });
 
 addBooksForm.addEventListener('submit', (e) => {
   e.preventDefault();
-  const newBook = new Object();
+  const newBook = {};
   const titleInput = document.getElementById('title');
   const authorInput = document.getElementById('author');
   newBook.title = titleInput.value;
